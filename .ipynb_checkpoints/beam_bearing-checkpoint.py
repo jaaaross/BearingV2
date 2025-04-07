5ca388d1d4dd0425668a2b18d5d42eaf8914642c
Jordan Ross <168299840+jaaaross@users.noreply.github.com>
2025-04-06 22:23:24 -0400

    test

--- .ipynb_checkpoints/beam_bearing-checkpoint.py
+++ .ipynb_checkpoints/beam_bearing-checkpoint.py
@@ -62,7 +62,7 @@ def run_check(node_df):
 #output_df = node_df.copy()
 
 #output_df.drop(output_df.columns[[3,4,8,9]], axis=1, inplace=True)
-
+'''
 b1_nonfire_capacity = []
 b2_nonfire_capacity = []
 
@@ -248,7 +248,7 @@ with col2:
 
 
 
-
+'''
 
 
 
