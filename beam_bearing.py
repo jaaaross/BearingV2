544efe8ca7a16980c82e269743868ec33b97eab3
Jordan Ross <168299840+jaaaross@users.noreply.github.com>
2025-04-12 22:28:46 -0400

    4

--- beam_bearing.py
+++ beam_bearing.py
@@ -62,7 +62,7 @@ st.write(df_capacity)
 
 output_df = node_df.copy()
 
-output_df.drop(output_df.columns[[1,2,3,4,5,6,7,8,9,10,11,12,13,14]], axis=1, inplace=True)
+output_df.drop(output_df.columns[[4,5,9,10], axis=1, inplace=True)
 
 b1_nonfire_capacity = []
 b2_nonfire_capacity = []
@@ -71,8 +71,8 @@ for row in df_capacity:
     b1_nonfire_capacity.append(row[0][0])
     b2_nonfire_capacity.append(row[1][0])
 
-output_df.insert(3, "B1 Nonfire Case Capacity", b1_nonfire_capacity, True)
-output_df.insert(11, "B2 Nonfire Case Capacity", b2_nonfire_capacity, True)
+output_df.insert(1, "B1 Nonfire Case Capacity", b1_nonfire_capacity, True)
+output_df.insert(1, "B2 Nonfire Case Capacity", b2_nonfire_capacity, True)
 
 st.write(output_df)
 
