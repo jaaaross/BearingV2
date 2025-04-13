f25ae8a30228d90ca3ffb06940c1f18c572f2ea5
Jordan Ross <168299840+jaaaross@users.noreply.github.com>
2025-04-12 23:50:32 -0400

    h

--- beam_bearing.py
+++ beam_bearing.py
@@ -92,17 +92,17 @@ for row in df_capacity:
 
 capacities_df.insert(1, "B1 Factored Load", b1_factored_load, True)
 capacities_df.insert(2, "B1 Nonfire Case Capacity", b1_nonfire_capacity, True)
-capacities_df.insert(3, "B1 Nonfire Case Ratio", b1_factored_load/b1_nonfire_capacity, True)
+capacities_df.insert(3, "B1 Nonfire Case Ratio", (b1_factored_load/b1_nonfire_capacity), True)
 capacities_df.insert(4, "B1 Unfactored Load", b1_unfactored_load, True)
 capacities_df.insert(5, "B1 Fire Case Capacity", b1_fire_capacity, True)
-capacities_df.insert(6, "B1 Fire Case Ratio", b1_unfactored_load/b1_fire_capacity, True)
+capacities_df.insert(6, "B1 Fire Case Ratio", (b1_unfactored_load/b1_fire_capacity), True)
 
 capacities_df.insert(7, "B2 Factored Load", b2_factored_load, True)
 capacities_df.insert(8, "B2 Nonfire Case Capacity", b2_nonfire_capacity, True)
-capacities_df.insert(9, "B2 Nonfire Case Ratio", b2_factored_load/b2_nonfire_capacity, True)
+capacities_df.insert(9, "B2 Nonfire Case Ratio", (b2_factored_load/b2_nonfire_capacity), True)
 capacities_df.insert(10, "B2 Unfactored Load", b2_unfactored_load, True)
 capacities_df.insert(11, "B2 Fire Case Capacity", b2_fire_capacity, True)
-capacities_df.insert(12, "B2 Fire Case Ratio", b2_unfactored_load/b2_fire_capacity, True)
+capacities_df.insert(12, "B2 Fire Case Ratio", (b2_unfactored_load/b2_fire_capacity), True)
 
 st.write(capacities_df)
 
